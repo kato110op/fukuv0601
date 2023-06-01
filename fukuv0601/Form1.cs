0e4d387737aba6aab14b851db5498bcb2858cec5
kato <130636687+kato110op@users.noreply.github.com>
2023-06-01 13:53:17 +0900

    Update Form1.cs

--- fukuv0601/Form1.cs
+++ fukuv0601/Form1.cs
@@ -3,6 +3,7 @@ namespace fukuv0601
     public partial class Form1 : Form
     {
         int vx = 10;
+        string chr = "BAKA";
         public Form1()
         {
             InitializeComponent();
@@ -10,17 +11,24 @@ namespace fukuv0601
 
         private void button1_Click(object sender, EventArgs e)
         {
-             vx = -10;
+             vx -= 10;
+             label1.Text = "KAMEHAMEHA";
+             
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-             vx = +10;
+             vx = 10;
+            label1.Text = "LOL";
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Left += vx;
+
+            string t = label1.Text;
+            label1.Text = chr;
+            chr = t;
         }
 
         private void label1_Click(object sender, EventArgs e)
