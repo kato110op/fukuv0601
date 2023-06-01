0fc879510edc122d3e0763a461564545b2c6a63a
kato <130636687+kato110op@users.noreply.github.com>
2023-06-01 16:11:11 +0900

    時は加速する

--- fukuv0601/Form1.cs
+++ fukuv0601/Form1.cs
@@ -30,25 +30,30 @@ namespace fukuv0601
             if (label1.Left < 0)
             {
                 vx = Math.Abs(vx + (vx / 10));
+                vy = vy + (vy / 10);
             }
              else if (label1.Right > ClientSize.Width)
             {
                 vx = -Math.Abs(vx + (vx / 10));
+                vy = vy + (vy / 10);
                 //vx =-10;
             }
             if (label1.Top < 0)
             {
                 vy = Math.Abs(vy + (vy / 10));
+                vy = vy - (vy / 10);
                 //vy = 10;
             }
             if (label1.Right > 800)
             {
                 vx = -Math.Abs(vx + (vx / 10));
+                vy = vy - (vy / 10);
                 //vx = -10;
             }
             if (label1.Bottom > ClientSize.Height)
             {
                 vy = -Math.Abs(vy + (vy / 10));
+                vy = vy - (vy / 10);
                 //vy = -10;
             }
             string t = label1.Text;
