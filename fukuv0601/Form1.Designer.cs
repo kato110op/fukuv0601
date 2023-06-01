bec5573195bacf463b67fe6d79094b3d9f34910b
kato <130636687+kato110op@users.noreply.github.com>
2023-06-01 15:41:08 +0900

    演習8_4

--- fukuv0601/Form1.Designer.cs
+++ fukuv0601/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(384, 231);
+            label1.Location = new Point(385, 440);
             label1.Name = "label1";
             label1.Size = new Size(38, 15);
             label1.TabIndex = 0;
--- fukuv0601/Form1.cs
+++ fukuv0601/Form1.cs
@@ -2,12 +2,14 @@ namespace fukuv0601
 {
     public partial class Form1 : Form
     {
-        int vx = - 10;
-        int vy = - 10;
+        int vx = -10;
+        int vy = -10;
+        int zettai = -10;
         string chr = "BAKA";
         public Form1()
         {
             InitializeComponent();
+            MessageBox.Show($"{label1.Right}");
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -18,14 +20,31 @@ namespace fukuv0601
 
         private void button2_Click(object sender, EventArgs e)
         {
-            vx = 10;
+            vx += 10;
             //label1.Text = "LOL";
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Left += vx;
-            label1.Top += vy;  
+            label1.Top += vy;
+            if (label1.Left < 0)
+            {
+                vx = 10;
+            }
+            if (label1.Top < 0)
+            {
+                vy = 10;
+                zettai++;
+            }
+            if (label1.Right > 800)
+            {
+                vx = -10;
+            }
+            if (label1.Bottom >450)
+            {
+                vy = -10;
+            }
             string t = label1.Text;
             label1.Text = chr;
             chr = t;
@@ -33,8 +52,9 @@ namespace fukuv0601
 
         private void label1_Click(object sender, EventArgs e)
         {
-            timer1.Stop();
+            //timer1.Stop();
             label1.Text = "KATO MASAHIRO";
+            zettai=+10;
         }
     }
 }
